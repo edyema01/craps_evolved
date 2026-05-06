e2915ba8733e289b4c8fbbbe8a5a9a711c62cb55
Yemane <essay.yemane@westpoint.edu>
2026-05-05 20:42:34 -0400

    fix templates folder

--- app2.py
+++ app2.py
@@ -20,6 +20,8 @@ def init_game():
     session["odds_bet"] = 0
 
 def roll_dice():
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
     return die1, die2
 
 def update_leaderboard(name, bankroll):
